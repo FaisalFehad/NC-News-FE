7eb5bb3d9f724b5b217ce00ee29ea860ba10bb7d
FaisalFehad <faisal.fehad@pm.me>
2020-01-20 14:30:02 +0000

    add id key for map func

--- fe-nc-news/src/components/ArticleCard.jsx
+++ fe-nc-news/src/components/ArticleCard.jsx
@@ -2,17 +2,15 @@ import React from "react";
 
 const ArticleCard = ({ allArticles }) => {
   if (allArticles) {
-    console.log(allArticles[0]);
-
     return allArticles.map(article => {
       return (
-        <div>
+        <main key={article.article_id}>
           <hr />
           <h3>Title: {article.title}</h3>
           <h3>Topic: {article.topic}</h3>
           <h3>Votes: {article.votes}</h3>
           <hr />
-        </div>
+        </main>
       );
     });
   }
