e45481899720d704a30bf69ef8ba6c2974fb9bc0
FaisalFehad <faisal.fehad@pm.me>
2020-01-23 12:26:06 +0000

    add boostrap noice to ErrDisplay

--- fe-nc-news/src/components/ErrDisplay.jsx
+++ fe-nc-news/src/components/ErrDisplay.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 
 const ErrHandling = ({ err_msg }) => {
-  if (err_msg) return <h3>{err_msg}</h3>;
   return (
-    <div>
-      <h3>Something went wrong... What have you done?</h3>
+    <div class="alert alert-danger" role="alert">
+      {err_msg ? <h3>{err_msg}</h3> : <h3>Page not found... 💩</h3>}
     </div>
   );
 };
