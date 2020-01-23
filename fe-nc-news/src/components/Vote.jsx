0f549a5f37183d161b7cc85da08fb648388c5f36
FaisalFehad <faisal.fehad@pm.me>
2020-01-23 14:52:37 +0000

    add bootstrap buttons

--- fe-nc-news/src/components/Vote.jsx
+++ fe-nc-news/src/components/Vote.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import updateArticleVoteReq from "../utils/updateVoteReq";
+import { Button } from "react-bootstrap";
 
 class Vote extends Component {
   state = { currentVote: 0, voteValue: 0 };
@@ -25,10 +26,24 @@ class Vote extends Component {
       <div>
         <h4>Votes: {this.state.currentVote}</h4>
         {this.state.voteValue <= 0 && (
-          <button onClick={event => this.handleVote(1)}>Up Vote 👍</button>
+          <button
+            onClick={event => this.handleVote(1)}
+            type="button"
+            class="btn btn-light"
+            type="button"
+            class="btn btn-outline-info"
+          >
+            Up Vote 👍
+          </button>
         )}
         {this.state.voteValue >= 0 && (
-          <button onClick={event => this.handleVote(-1)}>Down Vote 👎</button>
+          <button
+            onClick={event => this.handleVote(-1)}
+            type="button"
+            class="btn btn-outline-danger"
+          >
+            Down Vote 👎
+          </button>
         )}
         <br />
       </div>
